42f143b782b5288aa58e5d0050f0aa5dc3d41f02
bobbaicloudwithpants@gmail.com <bobbaicloudwithpants@gmail.com>
2021-01-28 18:59:52 +0800

    尝试解决跨域

--- src/index.js
+++ src/index.js
@@ -9,16 +9,26 @@ app.use(cors({
   origin: 'http://localhost:8080',    // 前端地址
   credentials: true
 }));
-app.use(async (ctx, next) => {
-  ctx.set('Access-Control-Allow-Origin', '*');
-  ctx.set('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
-  ctx.set('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
-  if (ctx.method == 'OPTIONS') {
-    ctx.body = 200;
-  } else {
-    await next();
-  }
-});
+app.use(cors({
+  origin: function () {
+    return 'http://localhost:8080';
+  },
+  exposeHeaders: ['WWW-Authenticate', 'Server-Authorization'],
+  maxAge: 5,
+  credentials: true,
+  allowMethods: ['GET', 'POST', 'DELETE', 'PATCH'], //设置允许的HTTP请求类型
+  allowHeaders: ['Content-Type', 'Authorization', 'Accept'],
+}));
+// app.use(async (ctx, next) => {
+//   ctx.set('Access-Control-Allow-Origin', '*');
+//   ctx.set('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
+//   ctx.set('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+//   if (ctx.method == 'OPTIONS') {
+//     ctx.body = 200;
+//   } else {
+//     await next();
+//   }
+// });
 app.use(bodyParser())
 app.use(logger)
 route(app)
